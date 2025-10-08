cd3fb87569dfd4440852cf03c24ea1ac738dd797
Igor Neves de Souza <54192837@sespa.local>
2025-10-08 18:11:25 -0300

    Primeiros ajustes na arquitetura do projeto

---
+++ instituicoes/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+# Você pode importar views depois
+# from . import views
+
+urlpatterns = [
+    # path('', views.index, name='index'),  # ← descomentar quando a view existir
+    # path('detalhe/<int:id>/', views.detalhe, name='detalhe'),
+    
+    # Por enquanto, deixamos vazio ou comentado
+]
